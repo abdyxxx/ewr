a6c756479a8f54dc05054e69c1aa11404dfcf4ac
abdyxxx <mmm.uuu1322@gmail.com>
2022-10-07 15:40:41 +0600

    07102022

--- src/components/SliderItemFunctional.js
+++ src/components/SliderItemFunctional.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { connect, useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import {  useDispatch, useSelector } from "react-redux";
 import { showDetailInform, selectCity, getCityData, getBackImg } from "../redux/actions";
 import Spinner from "./Spinner";
 
